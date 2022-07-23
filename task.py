4667a2b9a5f4a068db8daaaeba014c863be02757
unknown <6hassan@gmail.com>
2022-07-24 00:03:08 +1000

    yclib changed

--- task.py
+++ task.py
@@ -1,4 +1,10 @@
 from yclib import functions, reader
+import yaml
 
 a = functions.f1()
 print(a)
+
+with open('project_config.yaml') as f:
+    
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    print(data)
